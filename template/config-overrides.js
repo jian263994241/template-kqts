4a9f845487dd3d93ad1e71da04ac09fce3be95bf
jianl <jianjianfengyun@126.com>
2021-04-16 14:29:56 +0800

    fix: 🐛 fix

--- template/config-overrides.js
+++ template/config-overrides.js
@@ -1,42 +1,44 @@
 const path = require('path');
+const pkg = require('./package.json');
 const url = require('url');
 const FileManagerPlugin = require('filemanager-webpack-plugin');
+const WebpackBar = require('webpackbar');
 
 module.exports = function override(config, env) {
   //do stuff with the webpack config...
 
   const isProduction = process.env.NODE_ENV === 'production';
   const publicPath = url.parse(process.env.PUBLIC_URL).path;
-  const projectDir = path.basename(process.cwd());
+  const pkgDir = path.join(config.output.path, '../zip');
+
+  const zipCacheDir = path.join(
+    'node_modules/.cache/FileManagerPlugin/',
+    pkg.name,
+  );
+
+  config.plugins.push(new WebpackBar());
 
   if (isProduction) {
     config.plugins.push(
       new FileManagerPlugin({
         events: {
-          onStart: {
-            delete: [path.join(process.cwd(), '/.zip')],
-          },
           onEnd: {
             copy: [
               {
                 source: config.output.path,
-                destination: path.join(
-                  config.output.path,
-                  '../.zip',
-                  publicPath,
-                  projectDir,
-                ),
+                destination: path.join(zipCacheDir, publicPath),
               },
             ],
             archive: [
               {
-                source: path.join(process.cwd(), '/.zip'),
+                source: zipCacheDir,
                 destination: path.join(
-                  config.output.path,
-                  projectDir + '_' + Date.now() + '.zip',
+                  pkgDir,
+                  pkg.name + '_' + pkg.version + '.zip',
                 ),
               },
             ],
+            delete: [zipCacheDir],
           },
         },
       }),
