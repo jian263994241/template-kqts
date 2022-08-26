42ac5d0b0f2addbf087068a85e7060be250e7a5c
jianl <jianjianfengyun@126.com>
2022-08-26 14:35:16 +0800

    fix: 🐛 update

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,5 +1,5 @@
 import './reset.css';
-import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import 'regenerator-runtime/runtime';
 import React from 'react';
