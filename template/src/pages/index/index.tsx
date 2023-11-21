3dcdc8569bb51b6c2c059ec95a4216bd9126f219
jianl <jianjianfengyun@126.com>
2023-11-21 10:53:35 +0800

    Update index.tsx

--- template/src/pages/index/index.tsx
+++ template/src/pages/index/index.tsx
@@ -1,3 +1,19 @@
-export default () => {
-  return <div>index</div>;
+import { Helmet } from 'react-helmet-async';
+
+export async function loader() {
+  return {};
+}
+
+export default function PageIndex() {
+  return (
+    <>
+      <Helmet>
+        <title>index</title>
+      </Helmet>
+
+       <div>index</div>
+    </>
+  )
 };
+
+export const element = <PageIndex />;
