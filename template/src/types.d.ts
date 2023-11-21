0f07d5bbf403834d266b3b0ebd837c5cc0b6f9a2
jianl <jianjianfengyun@126.com>
2023-11-21 10:59:42 +0800

    Update types.d.ts

--- template/src/types.d.ts
+++ template/src/types.d.ts
@@ -1,1 +1,34 @@
 declare module 'react-stickky' {}
+
+declare global {
+  interface Window {
+    __ENV__: {
+      name: 'st02' | 'prod';
+    };
+    app: typeof router;
+    wx: any;
+    _hmt: {
+      push(
+        arg: [
+          '_trackEvent',
+          category: string,
+          action: string,
+          opt_label?: string,
+          opt_value?: number
+        ]
+      ): void;
+      push(arg: ['_trackPageview', pageURL: string]): void;
+    };
+  }
+
+  namespace JSX {
+    interface IntrinsicElements {
+      'wx-open-launch-weapp': {
+        username: string;
+        path: string;
+        ref?: React.RefObject<HTMLElement>;
+        children: React.ReactNode;
+      };
+    }
+  }
+}
