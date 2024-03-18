8bc1d1f00969fbd2224c1c0e01ad39bc49647fd8
jianl <jianjianfengyun@126.com>
2024-03-18 10:27:09 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src')
+    }
+  },
   server: {
     proxy: {
       '/api': {
