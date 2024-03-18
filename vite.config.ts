d9bab247e8aa0ad38f594b34567b1377020cb5e4
jianl <jianjianfengyun@126.com>
2024-03-18 16:20:25 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  envPrefix: 'REACT_',
   resolve: {
     alias: {
       src: path.resolve(__dirname, 'src')
