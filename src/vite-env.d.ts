960f7fc9c0d8c04353437acb362a0d6e24f508ad
jianl <jianjianfengyun@126.com>
2024-03-19 14:08:24 +0800

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly REACT_APP_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare global {
+  interface Window {
+    __ENV__: {
+      name: "st02" | "prod";
+    };
+  }
+}
