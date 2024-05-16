80ec8c95c5989199eacbdf1c732db01d59b154bc
jianl <jianjianfengyun@126.com>
2024-05-16 15:43:26 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path';
 export default defineConfig({
   base: '/',
   build: {
-    outDir: 'build',
+    outDir: 'dist',
     assetsInlineLimit: 1024,
     chunkSizeWarningLimit: 2000,
     rollupOptions: {
